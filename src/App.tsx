eb5be6ac140cd4f76144028e948294baa93c3672
Minh Nguyen <minh.n@hey.com>
2025-06-23 16:32:28 -0400

    PKMFE-12 basic flip card

--- src/App.tsx
+++ src/App.tsx
@@ -16,19 +16,24 @@ interface Message {
 }
 
 function App() {
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [guid, setGuid] = useState<string>(uuidv4());
+  const [game, setGame] = useState<any>(null);
+  const [cards, setCards] = useState<any[]>([]);
+  const [playerId, setPlayerId] = useState<string | null>(null);
+  const [opponentId, setOpponentId] = useState<string | null>(null);
 
   const { actionCable } = useActionCable(`ws://192.168.86.230:3000/cable`);
-  const { subscribe, unsubscribe, send } = useChannel<Message>(actionCable);
+  const { subscribe, unsubscribe, send } = useChannel<any>(actionCable);
 
   useEffect(() => {
+    if (!playerId) return;
     subscribe(
       { channel: "GamesChannel" },
       {
         received: (data) => {
           console.log("Received data:", data);
-          // setMessagesAndScrollDown([...messages, data]);
+          setGame(data.game);
+          setCards([...data.cards]);
+          setOpponentId(data.opponent || null);
         },
         connected: () => {
           send("get_game", {});
@@ -39,25 +44,19 @@ function App() {
     return () => {
       unsubscribe();
     };
-  }, []);
+  }, [playerId]);
 
   useEffect(() => {
-    fetchMessages();
+    getPlayerId();
   }, []);
 
-  const fetchMessages = async () => {
-    const response = await fetch("http://192.168.86.230:3000/messages", {
+  const getPlayerId = async () => {
+    const response = await fetch("http://192.168.86.230:3000/players/get_id", {
       credentials: "include",
     });
     const data = await response.json();
-    setMessagesAndScrollDown(data);
-  };
-
-  const setMessagesAndScrollDown = (newMessages: Message[]) => {
-    setMessages(newMessages);
-    setTimeout(() => {
-      window.scrollTo(0, document.body.scrollHeight);
-    }, 100);
+    console.log("Player ID:", data);
+    setPlayerId(data.id);
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -69,24 +68,47 @@ function App() {
 
     form.reset();
 
-    send("receive", { body: message, guid: guid });
+    send("receive", { body: message, playerId });
+  };
+
+  const flipCard = async (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.currentTarget.dataset.flipped === "true") return;
+    send("flip_card", {
+      game_card_id: e.currentTarget.id,
+      player_id: playerId,
+    });
   };
 
   return (
     <>
       <div className="App">
-        <div>guid: {guid}</div>
-        <h3>Messages</h3>
-        <div>
-          {messages.map((message) => (
-            <div key={message.id}>{message.body}</div>
+        <div>you: {playerId}</div>
+        {opponentId && <div>opponent: {opponentId}</div>}
+        <div>game id: {game?.id}</div>
+        <div>game state: {game?.state}</div>
+        <h3>Cards</h3>
+        <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+          {cards?.map((card: any, index) => (
+            <div
+              key={index}
+              id={card.id}
+              style={{
+                border: "1px solid #ccc",
+                borderRadius: "5px",
+                padding: "10px",
+                width: "50px",
+                height: "75px",
+                backgroundColor: card.flipped ? "#686868" : "#fff",
+              }}
+              data-flipped={card.flipped}
+              onClick={flipCard}
+            >
+              {card.card?.number && (
+                <div style={{ fontWeight: "bold" }}>{card.card.number}</div>
+              )}
+            </div>
           ))}
         </div>
-
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="message" />
-          <button type="submit">Send</button>
-        </form>
       </div>
     </>
   );
